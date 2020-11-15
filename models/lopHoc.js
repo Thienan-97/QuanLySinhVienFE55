2f985258733fa06af996ac1238df5fbb16f7ba16
unknown <phongpowerpoint@gmail.com>
2020-11-15 10:21:45 +0700

    version3_them_loPHoc

---
+++ models/lopHoc.js
@@ -0,0 +1,9 @@
+var LopHoc = function () {
+    this.maLop = '';
+    this.tenLop = '';
+    this.danhSachSinhVien =[];
+    this.hienThiThongTinLopHoc = (){
+        console.log(this.tenLop);
+
+    }
+}
