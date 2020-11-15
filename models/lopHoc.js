161ac5a74013b857efc09da7764e8051819804c1
unknown <phongpowerpoint@gmail.com>
2020-11-15 10:47:02 +0700

    devB_chinh_sua_sinh_vien

--- models/lopHoc.js
+++ models/lopHoc.js
@@ -2,8 +2,12 @@ var LopHoc = function () {
     this.maLop = '';
     this.tenLop = '';
     this.danhSachSinhVien =[];
-    this.hienThiThongTinLopHoc = (){
+    this.hienThiThongTinLopHoc = function (){
         console.log(this.tenLop);
-
+        console.log(this.tenLop);
+        console.log(this.danhSachSinhVien);
+    }
+    this.chinhSuaSinhVine = function(){
+        console.log('chinh sua sinh vien');
     }
 }
